9049c644eaa9ebec22d599d48054d11a9427d329
Caroline Richard <rchrdcaroline@gmail.com>
2020-06-11 16:07:04 +0200

    Recipe

---
+++ src/Entity/Recipe.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\RecipeRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=RecipeRepository::class)
+ */
+class Recipe
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $Title;
+
+    /**
+     * @ORM\Column(type="text", nullable=true)
+     */
+    private $SubTitle;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->Title;
+    }
+
+    public function setTitle(string $Title): self
+    {
+        $this->Title = $Title;
+
+        return $this;
+    }
+
+    public function getSubTitle(): ?string
+    {
+        return $this->SubTitle;
+    }
+
+    public function setSubTitle(?string $SubTitle): self
+    {
+        $this->SubTitle = $SubTitle;
+
+        return $this;
+    }
+}
