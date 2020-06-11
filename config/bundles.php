ec50736f81d1c0a403fb73ba39972a23263865b9
Caroline Richard <rchrdcaroline@gmail.com>
2020-06-11 14:41:46 +0200

    Add doctrine

--- config/bundles.php
+++ config/bundles.php
@@ -3,4 +3,5 @@
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true, 'test' => true],
+    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
 ];
