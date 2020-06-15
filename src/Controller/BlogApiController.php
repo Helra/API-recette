a00964e83d76e20cb3f9677621de2b7dadb4d388
Caroline Richard <rchrdcaroline@gmail.com>
2020-06-16 00:50:37 +0200

    try edit + sup

--- src/Controller/BlogApiController.php
+++ src/Controller/BlogApiController.php
@@ -140,4 +140,120 @@ class BlogApiController extends AbstractController
         return $this->json(["id" => $id, "Title Recipe" => $titleRecipe, "SubTitle Recipe" => $subTitleRecipe, "ingredients" => $nameIngredients]);
     }
 
+    /**
+     * Edit Ingredient
+     * @Route("/ingredients/{id}", name="IngredientEdit", methods={"POST"})
+     * @param int $id
+     * @param Request $request
+     * @param IngredientRepository $ingredientRepository
+     * @return JsonResponse
+     */
+    public function IngredientEdit( int $id, Request $request, IngredientRepository $ingredientRepository)
+    {
+        $ingredient = $ingredientRepository->find($id);
+
+        $datas = json_decode($request->getContent(), true);
+        $form = $this->createForm(IngredientType::class, $ingredient);
+        $form->submit($datas);
+
+        if ($form->isSubmitted()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($ingredient);
+            $em->flush();
+        }
+
+        $id = $ingredient->getId();
+        $name = $ingredient->getName();
+        $titles = $ingredient->getRecipes();
+        $recipes =[];
+        foreach ($titles as $title) {
+            $recipes =$title->getTitle();
+        }
+
+        return $this->json(['id' => $id, 'name' => $name, 'Recipe' => $recipes]);
+    }
+
+    //Voir quoi faire des ingrédients
+
+    /**
+     * Edit Recipe
+     * @Route("/recipes/{id}", name="RecipeEdit", methods={"POST"})
+     * @param RecipeRepository $recipeRepository
+     * @param int $id
+     * @param Request $request
+     * @param IngredientRepository $ingredientRepository
+     * @return JsonResponse
+     */
+    public function RecipeEdit(RecipeRepository $recipeRepository, int $id, Request $request, IngredientRepository $ingredientRepository)
+    {
+        $recipe = $recipeRepository->find($id);
+
+        $datas = json_decode($request->getContent(), true);
+        $form = $this->createForm(RecipeType::class, $recipe);
+        $form->handleRequest($request);
+
+        if (isset($datas['Ingredient'])) {
+            $dataForms = $datas['Ingredient'];
+
+            foreach ($dataForms as $key => $dataForm) {
+                if ($ingredientRepository->findOneByName($dataForm['name'])) {
+                    $dataForms[$key] = $ingredientRepository->findOneByName($dataForm['name']);
+                    $form->get('Ingredient')->setData($dataForm[$key]);
+                } else {
+                    $dataForms[$key] = $datas['Ingredient'][$key];
+                }
+            }
+        } else {
+            $dataForms = $recipe->getIngredient();
+        }
+
+        $datas['Ingredient'] = $dataForms;
+
+        $form->submit($datas);
+
+
+        if ($form->isSubmitted()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($recipe);
+            $em->flush();
+        }
+
+        $id = $recipe->getId();
+
+        $title = $recipe->getTitle();
+        $subTitle = '';
+        if ($recipe->getSubTitle()) {
+            $subTitle = $recipe->getSubTitle();
+        }
+        $nameIngredients = [];
+        $ingredients = $recipe->getIngredient();
+        foreach ($ingredients as $ingredient) {
+            $nameIngredients[] = $ingredient->getName();
+        }
+
+        return $this->json(['id' => $id, 'Title' => $title, 'SubTitle' => $subTitle, "Ingredients" => $nameIngredients]);
+    }
+
+    /**
+     * Delete Recipe
+     * @Route("/recipes/{id}", name="RecipeDelete", methods={"DELETE"})
+     * @param RecipeRepository $recipeRepository
+     * @param int $id
+     * @param Request $request
+     * @return JsonResponse
+     */
+    public function RecipeDelete(RecipeRepository $recipeRepository, int $id, Request $request)
+    {
+        $recipe = $recipeRepository->find($id);
+
+        if ($this->isCsrfTokenValid('delete'.$recipe->getId(), $request->request->get('_token'))) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->remove($recipe);
+            $entityManager->flush();
+            return $this->json(['Recette effacée']);
+        }
+
+    }
+
+
 }
